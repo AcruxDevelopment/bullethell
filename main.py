c6d49b4d94fad845a9038888448d4d78e6a68386
AcruxDelvelopment <acruxstudios.dev@gmail.com>
2025-08-07 10:06:09 -0300

    Final attack

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import math
 from object import GameObject
 from textures import Textures
 from bullet_heart_spinner import BulletHeartSpinner
@@ -14,6 +15,7 @@ clock = pygame.time.Clock()
 
 # --- Create objects ---
 soul = Soul(WIDTH//2, HEIGHT//2)
+root = GameObject(WIDTH//2, HEIGHT//2, 0, None)
 bullets = []
 
 # --- Main Loop ---
@@ -30,13 +32,20 @@ while running:
     keys = pygame.key.get_pressed()
 
     if frame % 8 == 0:
+        x = math.cos(frame*0.005)*50
+        #x = 0
         #bullets.append(BulletHeartSpinner(0, (HEIGHT // 2) + (random.randint(-2, 2) * 120), 0, random.randint(3, 4)))
         #bullets.append(BulletRain(random.randint(0,16) * 64, HEIGHT, 180, 0.05, random.randint(-10, 10)/10))
         #bullets.append(BulletRain(0, random.randint(0,8) * 64, 90, 0.05, random.randint(-10, 10)/10))
-        bullets.append(BulletRain(WIDTH//2, HEIGHT//2, deg, 0.1, random.randint(-10, 10)/10))
-        bullets.append(BulletRain(WIDTH//2, HEIGHT//2, deg+127, 0.1, random.randint(-10, 10)/10))
-        bullets.append(BulletRain(WIDTH//2, HEIGHT//2, deg-127, 0.1, random.randint(-10, 10)/10))
-        deg += 20
+        bullets.append(BulletRain(WIDTH//2+x, HEIGHT//2, deg, 0.08))
+        root.add_child(bullets[-1])
+        bullets.append(BulletRain(WIDTH//2+x, HEIGHT//2, deg+127, 0.08))
+        root.add_child(bullets[-1])
+        bullets.append(BulletRain(WIDTH//2+x, HEIGHT//2, deg-127, 0.08))
+        root.add_child(bullets[-1])
+        root.x = x
+        deg += 25
+
 
     # Remove bullets off screen
     bullets = [b for b in bullets if not b.is_off_screen(WIDTH, HEIGHT)]
@@ -44,6 +53,7 @@ while running:
     for bullet in bullets:
         bullet.update()
         bullet.draw(screen)
+        bullet.degree += 0.7
 
     if keys[pygame.K_LEFT]:
         soul.move_by(-soul.vel, 0)
@@ -66,6 +76,6 @@ while running:
 
     pygame.display.flip()
     clock.tick(60)
-    frame += 1
+    frame += 4
 
 pygame.quit()
